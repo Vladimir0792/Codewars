b8275e7edc2428fa77c0a8c5295dfc6551cbe245
Vladimir0792 <vova19927@mail.ru>
2023-05-01 10:06:12 +0300

    maps
    Signed-off-by: Vladimir0792 <vova19927@mail.ru>

---
+++ LostWithoutMap/Map.java
@@ -0,0 +1,15 @@
+package LostWithoutMap;
+
+public class Map {
+    public static int[] map(int[] arr) {
+        for(int i = 0;i<arr.length; i++){
+            arr[i]=arr[i]*2;
+        }
+        return arr;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
